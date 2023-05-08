2c7a2af01f06f6e2c5e262f23d530a7ec89c15a1
paundra febrian wijaya <paundrafebrianwijaya@gmail.com>
2023-05-08 16:06:51 +0700

    Initial commit

---
+++ app/src/main/java/com/example/cardview_sqlite_kotlin_modul6/DBModel.kt
@@ -0,0 +1,8 @@
+package com.example.cardview_sqlite_kotlin_modul6
+
+data class DBModel  (
+    var email: String = "",
+    var pass: String = "",
+    var username: String = "",
+    var fullname: String = ""
+)
